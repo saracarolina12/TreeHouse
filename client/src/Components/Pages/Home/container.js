ea20b012c5485161c5f2cd5a7d6a50eb39e06d4c
LuisR-jpg <laloro08@hotmail.com>
2022-01-30 19:50:21 -0600

    30 de Enero

--- client/src/Components/Pages/Home/container.js
+++ client/src/Components/Pages/Home/container.js
@@ -11,9 +11,23 @@ import Nav from 'react-bootstrap/Nav'
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import logo from './TreeHouse.mp4'
+import { Parallax, Background } from "react-parallax";
 
+  const insideStyles = {
+    // background: "white",
+    // padding: 20,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)"
+  };
 
 function Cont() {
+
+    const image4 = "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg";
+    const image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJxbiScO3PX80CgzWk5uTYQS7BHAh9vOWJ2g&usqp=CAU";
+    const image1 = "https://cdn.pixabay.com/photo/2016/06/02/02/31/background-1430103_960_720.png";
+
 
     const menuChange = (x) =>{
         console.log("menuuu");
@@ -37,43 +51,68 @@ function Cont() {
     }
 
     return (
-        <div>
-            <video autoPlay muted id='video' >
-                <source src={logo} type='video/mp4'/>
-            </video>
-            <Container fluid className="container">
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
-                <br/><br/>
+        <div className="cont"> 
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            {/* <Container fluid className="container"> */}
 
-                <h1 className="Titulo">¡Bienvenid@!</h1>
-                <br/><br/>
-                <Button onClick={menuChange} className="menueventos" variant="success">Menú</Button>
-                <br/><br/>
-                <Button onClick={eventosChange} className="menueventos" variant="success">Eventos</Button>
-                <br/><br/>
-                <br/><br/>
-
-
-                <i class="small material-icons right iconColor" onClick={iconChange} >info_outline</i>
+                <Parallax
+                    bgImage={image1}
+                    strength={300}
+                    renderLayer={(percentage) => (
+                        <div>
+                            <video autoPlay muted id='video' style={{opacity: 1.2 - percentage}}>
+                                <source src={logo} type='video/mp4'/>
+                            </video>
+                            <div
+                                style={{
+                                    position: "absolute",
+                                    background: `rgba(235, 229, 210, ${percentage * 1})`,
+                                    left: "50%",
+                                    top: "50%",
+                                    borderRadius: "50%",
+                                    transform: "translate(-50%,-50%)",
+                                    width: percentage * 750,
+                                    height: percentage * 750
+                                }}
+                                />
+                        </div>
+                    )}
+                >
+                    <div style={{ height: 750 }}>
+                        <div style={insideStyles}>
+                            <h1 className="Titulo">¡Bienvenid@!</h1>
+                            <br/><br/>
+                            <Button onClick={menuChange} className="menueventos" variant="success">Menú</Button>
+                            <br/><br/>
+                            <Button onClick={eventosChange} className="menueventos" variant="success">Eventos</Button>
+                            <br/><br/>
+                            <br/><br/>
+                            <i class="small material-icons right iconColor" onClick={iconChange} >info_outline</i>
+                        </div>
+                    </div>
+                </Parallax>
             
                 <br/>
-            </Container>
+            {/* </Container> */}
         </div>
     )
 }
